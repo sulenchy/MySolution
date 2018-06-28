aa2f5d2bab8978a78c44162a10339244b3f4a787
sulenchy <sulaimanabiodun172@gmail.com>
2018-06-28 20:33:03 +0100

    add the static files

--- server/app.js
+++ server/app.js
@@ -1,7 +1,12 @@
 import express from 'express';
+import path from 'path';
 
 const app = express()
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.use(express.static('public'))
+
+app.get('/', (req, res) =>{
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'))
